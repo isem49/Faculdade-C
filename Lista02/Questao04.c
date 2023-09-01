d7a6f4dbf40436dfca870ea06da963f2b09ea30f
Júnior <56940605+isem49@users.noreply.github.com>
2023-09-01 15:00:49 -0300

    Add files via upload

---
+++ Lista02/Questao04.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+ 
+     int a,b,r; 
+
+    
+    printf("Digite o primeiro numero: ");
+    scanf("%d",&a);
+    printf("Digite o segundo numero: ");
+    scanf("%d",&b);
+
+	if(a > b){
+		
+		r = a - b;
+		printf("resultado e: %d ", r);
+	}else if (b > a){
+		
+		r = b - a;
+		printf("resultado e: %d", r);
+		
+	}else{
+	    
+	    	printf("Os valores são iguais ou seja o resultado e 0");
+	}
+
+	
+    return 0;
+   
+}
---
+++ Lista02/Questao05.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+ 
+    int a; 
+
+    
+    printf("Digite o  numero: ");
+    scanf("%i",&a);
+ 
+  	if(a % 4 == 0){
+  		
+  		printf("o numero e divisivel por 4 ");
+  	}else if (a % 5 == 0){
+  		
+  
+  		printf("o numero e divisivel por 5 ");
+  		
+  	}else{
+  	    
+  	    	printf("nao e divisivel por 4 e nem por 5");
+  	}
+  
+  	
+      return 0;
+   
+}
